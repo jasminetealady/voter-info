6481f41d02c867f4f770a38f284c8df874a0e250
Razelle McCarrick <razelle@satyaorthopedic.com>
2020-01-20 14:11:01 -0500

    cleanip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,61 @@
 <template>
   <div class="home">
     <div class="header w-full flex flex-col items-center justify-center py-10">
-      <h1 class="text-blue font-bold text-4xl font-body">Vote 2020</h1>
-      <div class="my-10 w-1/4 items-center flex justify-between h-10">
-        <input class="border shadow py-2 px-4 w-2/4" type="text" v-model="address" />
-        <button
-          @click="requestData()"
-          class="bg-red text-white rounded py-2 px-4"
-        >Retrieve Voter Information</button>
-      </div>
-      <div
-        v-if="contest.office"
-        v-for="contest in data.contests"
-        class="border bg-white rounded shadow-md Contest w-3/5 py-10 my-5"
-      >
-        <div class="w-full justify-center items-center flex flex-col py-5">
-          <h1 class="text-red text-xl font-bold">Race: {{contest.office}}</h1>
-          <h2 class="text-gray-700 text-xl font-bold">District: {{contest.district.name}}</h2>
+      <h1 class="text-blue font-bold text-4xl font-body">2020 Election Voter Information</h1>
+      <div class="w-full sm:w-8/12 md:w-6/12 px-4">
+        <div class="my-5 w-full items-center flex flex-col justify-between">
+          <input class="border shadow py-2 px-4 w-2/4 rounded" type="text" v-model="address" />
+          <button
+            @click="requestData()"
+            class="bg-red text-white rounded mt-4 py-2 px-4"
+          >Retrieve Voter Information</button>
         </div>
 
-        <div class="p-4 w-full justify-center flex flex-wrap">
-          <div
-            :class="partyColor(candidate.party)"
-            class="border text-center flex rounded py-12 flex-col justify-center items-center w-full p-2 mx-2 my-2"
-            v-for="candidate in contest.candidates"
-          >
-            <p class="text-xl text-white font-bold mb-4">{{candidate.name}}</p>
-            <a
-              v-if="candidate.phone"
-              class="text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor"
-              :href="`tel:+1-${candidate.phone}`"
-              target="_blank"
-            >Phone</a>
-            <a
-              v-if="candidate.email"
-              class="text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor"
-              :href="`mailto:${candidate.email}`"
-              target="_blank"
-            >Email</a>
-            <a
-              v-if="candidate.candidateUrl"
-              class="text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor"
-              :href="candidate.candidateUrl"
-              target="_blank"
-            >Website</a>
-            <a class="text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor" v-for="channel in candidate.channels" :href="channel.id">{{channel.type}}</a>
+        <!--Contest Boxes-->
+        <div
+          :key="contest.office"
+          v-if="contest.office"
+          v-for="contest in data.contests"
+          class="border bg-white rounded shadow-md w-full px-4 py-10 my-10 text-center"
+        >
+          <div class="w-full justify-center items-center flex flex-col py-5">
+            <h1 class="text-red text-2xl font-bold capitalize">{{contest.office}}</h1>
+            <h2 class="text-gray-700 text-lg font-bold capitalize mt-2">{{contest.district.name}}</h2>
+          </div>
+
+          <div class="w-full justify-center flex flex-wrap">
+            <div
+              :key="candidate.party"
+              :class="partyColor(candidate.party)"
+              class="border shadow text-center flex rounded py-12 flex-col justify-center items-center w-full p-2 mx-2 my-2"
+              v-for="candidate in contest.candidates"
+            >
+              <p class="text-xl text-white font-bold mb-4 capitalize">{{candidate.name}}</p>
+              <a
+                v-if="candidate.phone"
+                class="shadow text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor"
+                :href="`tel:+1-${candidate.phone}`"
+                target="_blank"
+              >Phone</a>
+              <a
+                v-if="candidate.email"
+                class="shadow text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor"
+                :href="`mailto:${candidate.email}`"
+                target="_blank"
+              >Email</a>
+              <a
+                v-if="candidate.candidateUrl"
+                class="shadow text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor hover:scale"
+                :href="candidate.candidateUrl"
+                target="_blank"
+              >Website</a>
+              <a
+                class="shadow text-gray-700 cursor bg-white rounded py-2 px-4 my-2 w-40 cursor"
+                :key="channel.type"
+                v-for="channel in candidate.channels"
+                :href="channel.id"
+              >{{channel.type}}</a>
+            </div>
           </div>
         </div>
       </div>
@@ -59,7 +70,7 @@ export default {
   data() {
     return {
       data: [],
-      address: '560 boylston st boston',
+      address: '1082 davol st',
     };
   },
   methods: {
